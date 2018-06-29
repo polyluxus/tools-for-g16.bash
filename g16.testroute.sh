0a82660f07bc9e83fdc516b2073fd85a32e6f7bd
Martin Schwarzer <polyluxus@gmail.com>
2018-06-29 11:38:23 +0200

    first working draft of testroute

---
+++ g16.testroute.sh
@@ -0,0 +1,269 @@
+#! /bin/bash
+
+# Gaussian 16 submission script
+#
+# You might not want to make modifications here.
+# If you do improve it, I would be happy to learn about it.
+#
+
+# 
+# The help lines are distributed throughout the script and grepped for
+#
+#hlp   This script reads an input file, extracts the route section,
+#hlp   and tests it with the Gaussian utility testrt.
+#hlp
+#hlp   This software comes with absolutely no warrenty. None. Nada.
+#hlp
+#hlp   Usage: $scriptname [options] [IPUT_FILE]
+#hlp
+
+#
+# Generic functions to find the scripts 
+# (Copy of ./resources/locations.sh)
+#
+# Let's know where the script is and how it is actually called
+#
+
+get_absolute_location ()
+{
+    # Resolves the absolute location of parameter and returns it
+    # Taken from https://stackoverflow.com/a/246128/3180795
+    local resolve_file="$1" description="$2" 
+    local link_target directory_name filename resolve_dir_name 
+    debug "Getting directory for '$resolve_file'."
+    #  resolve $resolve_file until it is no longer a symlink
+    while [[ -h "$resolve_file" ]]; do 
+      link_target="$(readlink "$resolve_file")"
+      if [[ $link_target == /* ]]; then
+        debug "File '$resolve_file' is an absolute symlink to '$link_target'"
+        resolve_file="$link_target"
+      else
+        directory_name="$(dirname "$resolve_file")" 
+        debug "File '$resolve_file' is a relative symlink to '$link_target' (relative to '$directory_name')"
+        #  If $resolve_file was a relative symlink, we need to resolve 
+        #+ it relative to the path where the symlink file was located
+        resolve_file="$directory_name/$link_target"
+      fi
+    done
+    debug "File is '$resolve_file'" 
+    filename="$(basename "$resolve_file")"
+    debug "File name is '$filename'"
+    resolve_dir_name="$(dirname "$resolve_file")"
+    directory_name="$(cd -P "$(dirname "$resolve_file")" && pwd)"
+    if [[ "$directory_name" != "$resolve_dir_name" ]]; then
+      debug "$description '$directory_name' resolves to '$directory_name'."
+    fi
+    debug "$description is '$directory_name'"
+    if [[ -z $directory_name ]] ; then
+      directory_name="."
+    fi
+    echo "$directory_name/$filename"
+}
+
+get_absolute_filename ()
+{
+    # Returns only the filename
+    local resolve_file="$1" description="$2" return_filename
+    return_filename=$(get_absolute_location "$resolve_file" "$description")
+    return_filename=${return_filename##*/}
+    echo "$return_filename"
+}
+
+get_absolute_dirname ()
+{
+    # Returns only the directory
+    local resolve_file="$1" description="$2" return_dirname
+    return_dirname=$(get_absolute_location "$resolve_file" "$description")
+    return_dirname=${return_dirname%/*}
+    echo "$return_dirname"
+}
+
+get_scriptpath_and_source_files ()
+{
+    local error_count tmplog line tmpmsg
+    tmplog=$(mktemp tmp.XXXXXXXX) 
+    # Who are we and where are we?
+    scriptname="$(get_absolute_filename "${BASH_SOURCE[0]}" "installname")"
+    debug "Script is called '$scriptname'"
+    # remove scripting ending (if present)
+    scriptbasename=${scriptname%.sh} 
+    debug "Base name of the script is '$scriptbasename'"
+    scriptpath="$(get_absolute_dirname  "${BASH_SOURCE[0]}" "installdirectory")"
+    debug "Script is located in '$scriptpath'"
+    resourcespath="$scriptpath/resources"
+    
+    if [[ -d "$resourcespath" ]] ; then
+      debug "Found library in '$resourcespath'."
+    else
+      (( error_count++ ))
+    fi
+    
+    # Import default variables
+    #shellcheck source=/home/te768755/devel/tools-for-g16.bash/resources/default_variables.sh
+    source "$resourcespath/default_variables.sh" &> "$tmplog" || (( error_count++ ))
+    
+    # Set more default variables
+    exit_status=0
+    stay_quiet=0
+    # Ensure that in/outputfile variables are empty
+    unset inputfile
+    unset outputfile
+    
+    # Import other functions
+    #shellcheck source=/home/te768755/devel/tools-for-g16.bash/resources/messaging.sh
+    source "$resourcespath/messaging.sh" &> "$tmplog" || (( error_count++ ))
+    #shellcheck source=/home/te768755/devel/tools-for-g16.bash/resources/rcfiles.sh
+    source "$resourcespath/rcfiles.sh" &> "$tmplog" || (( error_count++ ))
+    #shellcheck source=/home/te768755/devel/tools-for-g16.bash/resources/test_files.sh
+    source "$resourcespath/test_files.sh" &> "$tmplog" || (( error_count++ ))
+    #shellcheck source=/home/te768755/devel/tools-for-g16.bash/resources/process_gaussian.sh
+    source "$resourcespath/process_gaussian.sh" &> "$tmplog" || (( error_count++ ))
+    #shellcheck source=/home/te768755/devel/tools-for-g16.bash/resources/validate_numbers.sh
+    source "$resourcespath/validate_numbers.sh" &> "$tmplog" || (( error_count++ ))
+
+    if (( error_count > 0 )) ; then
+      echo "ERROR: Unable to locate library functions. Check installation." >&2
+      echo "ERROR: Expect functions in '$resourcespath'."
+      debug "Errors caused by:"
+      while read -r line || [[ -n "$line" ]] ; do
+        debug "$line"
+      done < "$tmplog"
+      tmpmsg=$(rm -v "$tmplog")
+      debug "$tmpmsg"
+      exit 1
+    else
+      tmpmsg=$(rm -v "$tmplog")
+      debug "$tmpmsg"
+    fi
+}
+
+#
+# Specific functions for this script only
+#
+
+validate_g16_route ()
+{
+    local read_route="$1"
+    local g16_output
+    debug "Read the following route section:"
+    debug "$readroute"
+    if g16_output=$($g16_testrt_cmd "$read_route" 2>&1) ; then
+      message "Route section has no syntax errors."
+      debug "$g16_output"
+    else
+      warning "There was an error in the route section"
+      message "$g16_output"
+      return 1
+    fi
+}
+
+process_inputfile ()
+{
+    local testfile="$1"
+    debug "Processing Input: $testfile"
+    validate_g16_route "$route_section"
+}
+
+#
+# Process Options
+#
+
+process_options ()
+{
+  ##Needs complete rework
+
+    #hlp   Options:
+    #hlp    
+    local OPTIND=1 
+
+    while getopts :sh options ; do
+        case $options in
+
+          #hlp     -s       Suppress logging messages of the script.
+          #hlp              (May be specified multiple times.)
+          #hlp
+            s) (( stay_quiet++ )) ;;
+
+          #hlp     -h       this help.
+          #hlp
+            h) helpme ;;
+
+           \?) fatal "Invalid option: -$OPTARG." ;;
+
+            :) fatal "Option -$OPTARG requires an argument." ;;
+
+        esac
+    done
+
+    # Shift all variables processed to far
+    shift $((OPTIND-1))
+
+    if [[ -z "$1" ]] ; then 
+      fatal "There is no inputfile specified"
+    fi
+
+    # If a filename is specified, it must exist, otherwise exit
+    # different mode let's you only use the jobname
+    #requested_inputfile=$(is_readable_file_or_exit "$1") || exit 1 
+    requested_inputfile="$1"
+    shift
+    debug "Specified input: $requested_inputfile"
+
+    # Issue a warning that the addidtional flag has no effect.
+    warn_additional_args "$@"
+}
+
+#
+# MAIN SCRIPT
+#
+
+# If this script is sourced, return before executing anything
+(( ${#BASH_SOURCE[*]} > 1 )) && return 0
+
+# Save how script was called
+script_invocation_spell="$0 $*"
+
+# Sent logging information to stdout
+exec 3>&1
+
+# Need to define debug function if unknown
+if ! command -v debug ; then
+  debug () {
+    echo "DEBUG  : " "$*" >&4
+  }
+fi
+
+# Secret debugging switch
+if [[ "$1" == "debug" ]] ; then
+  exec 4>&1
+  stay_quiet=0 
+  shift 
+else
+  exec 4> /dev/null
+fi
+
+get_scriptpath_and_source_files || exit 1
+
+# Check for settings in three default locations (increasing priority):
+#   install path of the script, user's home directory, current directory
+g16_tools_rc_loc="$(get_rc "$scriptpath" "/home/$USER" "$PWD")"
+debug "g16_tools_rc_loc=$g16_tools_rc_loc"
+
+# Load custom settings from the rc
+
+if [[ ! -z $g16_tools_rc_loc ]] ; then
+  #shellcheck source=/home/te768755/devel/tools-for-g16.bash/g16.tools.rc 
+  . "$g16_tools_rc_loc"
+  message "Configuration file '$g16_tools_rc_loc' applied."
+else
+  debug "No custom settings found."
+fi
+
+# Evaluate Options
+
+process_options "$@"
+process_inputfile "$requested_inputfile"
+
+#hlp   $scriptname is part of $softwarename $version ($versiondate) 
+message "$scriptname is part of $softwarename $version ($versiondate)"
+debug "$script_invocation_spell"
