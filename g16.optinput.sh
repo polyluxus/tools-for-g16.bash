polyluxus/tools-for-g16.bash

commit cc6436eaba9c0f440b11aee498e323f8fbe37bb3

implement option to write to specific file name for optinput (not derive it from input file)

g16.optinput.sh
@@ -229,8 +229,10 @@ process_inputfile ()
     # Assign new checkpoint/inputfile 
     use_file_suffix="opt"
     jobname="${jobbasename}.$use_file_suffix"
-    checkpoint="${jobname}.chk"
-    inputfile="${jobname}.com"
+
+    [[ -z $inputfile ]] && inputfile="${jobname}.com"
+    checkpoint="${inputfile%.*}.chk"
+
     backup_if_exists "$inputfile"
 
     local concatenate_opt_opts opt_keyword
@@ -259,7 +261,7 @@ process_options ()
     #hlp    
     local OPTIND=1 
 
-    while getopts :o:r:t:m:p:d:sh options ; do
+    while getopts :o:r:t:f:m:p:d:sh options ; do
         case $options in
           #hlp   -o <ARG>   Adds options <ARG> to the opt keyword.
           #hlp              May be specified multiple times.
@@ -283,6 +285,12 @@ process_options ()
           #hlp 
           t) 
             use_custom_tail[${#use_custom_tail[@]}]="$OPTARG" 
+            ;;
+
+          #hlp   -f <ARG>   Write inputfile to <ARG>.
+          #hlp
+          f)
+            inputfile="$OPTARG"
             ;;
 
           # Link 0 related options

commit d35cdb39c00ef0e55564d9e91a32649ee7c0c483

fixed error introduced in commit cc6436eaba9c0f440b11aee498e323f8fbe37bb3 as the target inputfile was not properly specified

g16.optinput.sh
@@ -230,10 +230,10 @@ process_inputfile ()
     use_file_suffix="opt"
     jobname="${jobbasename}.$use_file_suffix"
 
-    [[ -z $inputfile ]] && inputfile="${jobname}.com"
-    checkpoint="${inputfile%.*}.chk"
+    [[ -z $inputfile_new ]] && inputfile_new="${jobname}.$g16_input_suffix"
+    checkpoint="${inputfile_new%.*}.chk"
 
-    backup_if_exists "$inputfile"
+    backup_if_exists "$inputfile_new"
 
     local concatenate_opt_opts opt_keyword
     if (( ${#use_opt_opts[@]} == 0 )) ; then
@@ -247,8 +247,8 @@ process_inputfile ()
 
     route_section="$modified_route $opt_keyword"
 
-    write_g16_input_file > "$inputfile"
-    message "Written modified inputfile '$inputfile'."
+    write_g16_input_file > "$inputfile_new"
+    message "Written modified inputfile '$inputfile_new'."
 }
 
 #
@@ -290,7 +290,8 @@ process_options ()
           #hlp   -f <ARG>   Write inputfile to <ARG>.
           #hlp
           f)
-            inputfile="$OPTARG"
+            inputfile_new="$OPTARG"
+            debug "Setting inputfile='$inputfile'."
             ;;
 
           # Link 0 related options