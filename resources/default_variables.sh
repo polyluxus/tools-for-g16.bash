c01d8c2f8c1eb841a39d5e039ead62e154ccf25e
Martin Schwarzer <polyluxus@gmail.com>
2019-02-05 14:07:56 +0100

    Bump version to 0.1.1 (a.k.a. Aadrienne[one])

--- resources/default_variables.sh
+++ resources/default_variables.sh
@@ -13,8 +13,8 @@ fi
 # Generic details about these tools 
 #
 softwarename="tools-for-g16.bash"
-version="0.1.0"
-versiondate="2019-02-04"
+version="0.1.1"
+versiondate="2019-02-05"
 
 #
 # Standard commands for external software:
