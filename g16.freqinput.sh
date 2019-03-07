bc13ec0e7ba809964bedd419a40bafcace7b54be
Martin Schwarzer <polyluxus@gmail.com>
2019-03-07 14:01:44 +0100

    freqinput filters out the freq suffix if present, this avoids having something.freq.freq.something, which is mentioned in #68

--- g16.freqinput.sh
+++ g16.freqinput.sh
@@ -260,9 +260,9 @@ process_inputfile ()
       use_file_suffix="${file_suffix_temp}_${file_suffix_pres}"
     fi
     if [[ -z $use_file_suffix ]] ; then
-      jobname="${jobname}.freq"
+      jobname="${jobname%.freq}.freq"
     else
-      jobname="${jobname}.freq.$use_file_suffix"
+      jobname="${jobname%.freq}.freq.$use_file_suffix"
     fi
 
     [[ -z $inputfile_new ]] && inputfile_new="${jobname}.$g16_input_suffix"
