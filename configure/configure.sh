60bb30372dff2db1155c80599c97f8a1a6100fec
Martin Schwarzer <polyluxus@gmail.com>
2019-04-29 00:42:44 +0200

    Added: Experimental route builder for configuration #79

--- configure/configure.sh
+++ configure/configure.sh
@@ -421,68 +421,72 @@ ask_g16_default_extensions ()
 
 ask_g16_store_route_section ()
 {
-  local user_input array_index array_index_current
-  local -a tmp_g16_route_section_predefined
-  while true ; do
-    if (( ${#use_g16_route_section_predefined[@]} != 0 )) ; then
-      printf '%5s : %s\n' "Index" "Predefined route section"
-      array_index=0
+  if [[ -x "$scriptpath/routebuilder.sh" ]] ; then
+    debug "Calling the route builder script."
+    local tmpfile_routes_tomodify
+    tmpfile_routes_tomodify="$( mktemp --tmpdir )"
+    {
+      local array_index=0
       for array_index in "${!use_g16_route_section_predefined[@]}" ; do
-        printf '%5d : %s\n' "$array_index" "${use_g16_route_section_predefined[$array_index]}"
-        array_index_current=$(( array_index + 1 ))
+        printf '  g16_route_section_predefined[%d]="%s"\n' "$array_index" "${use_g16_route_section_predefined[$array_index]}"
+        printf '  g16_route_section_predefined_comment[%d]="%s"\n' "$array_index" "${use_g16_route_section_predefined_comment[$array_index]}"
       done
-    fi
-    ask "Would you like to change these settings?"
-    message "To delete an entry, enter 'del <number>',"
-    message "to add or replace an entry, enter 'add <number>',"
-    message "where <number> is optional."
-    user_input=$(read_human_input)
-    [[ -z $user_input ]] && break
-    if [[ "$user_input" =~ ^[[:space:]]*[Dd][Ee][Ll][[:space:]]*([0-9]*)[[:space:]]*$ ]] ; then
-      if [[ -z ${BASH_REMATCH[1]} ]] ; then
-        ask "Which entry would you like to delete?"
-        array_index=$(read_integer)
-      else
-        array_index="${BASH_REMATCH[1]}"
-      fi
-      unset 'use_g16_route_section_predefined[array_index]'
-    elif [[ "$user_input" =~ ^[[:space:]]*[Aa][Dd][Dd][[:space:]]*([0-9]*)[[:space:]]*$ ]] ; then
-      if [[ -z ${BASH_REMATCH[1]} ]] ; then
-        array_index="$array_index_current"
-        while array_index in "${!use_g16_route_section_predefined[@]}" ; do
-          (( array_index++ ))
-        done
-      else
-        array_index="${BASH_REMATCH[1]}"
-      fi
-      ask "What should the new route section be?"
-      user_input=$(read_human_input)
-      use_g16_route_section_predefined[$array_index]="$user_input"
+    } > "$tmpfile_routes_tomodify"
+    local tmpfile_routes_edited
+    tmpfile_routes_edited="$( mktemp --tmpdir )"
+    debug "Created temporary file: $tmpfile_route_edited"
+    if [[ "$debugging" == "true" ]] ; then
+      "$scriptpath/routebuilder.sh" "debug" -r "$tmpfile_routes_tomodify" -o "$tmpfile_routes_edited" --
     else
-      warning "Unrecognised command."
+      "$scriptpath/routebuilder.sh" -r "$tmpfile_routes_tomodify" -o "$tmpfile_routes_edited" -- 
     fi
-    unset user_input array_index
-  done
-  tmp_g16_route_section_predefined=( "${use_g16_route_section_predefined[@]}" )
-  if (( ${#tmp_g16_route_section_predefined[@]} != 0 )) ; then
-    array_index=0
-    printf '%5s : %s\n' "Index" "Predefined route section"
-    for array_index in "${!tmp_g16_route_section_predefined[@]}" ; do
-      printf '%5d : %s\n' "$array_index" "${tmp_g16_route_section_predefined[$array_index]}"
+    debug "Received: $( cat "$tmpfile_routes_edited" )"
+    debug "Resetting routevariables."
+    unset use_g16_route_section_predefined use_g16_route_section_predefined_comment
+    unset     g16_route_section_predefined     g16_route_section_predefined_comment
+    debug "Sourceing '$tmpfile_routes_edited'."
+    . "$tmpfile_routes_edited"
+    debug "$( rm -v -- "$tmpfile_routes_edited" )"
+    use_g16_route_section_predefined=( "${g16_route_section_predefined[@]}" )
+    use_g16_route_section_predefined_comment=( "${g16_route_section_predefined_comment[@]}" )
+    debug "$(declare -p use_g16_route_section_predefined)"
+    debug "$(declare -p use_g16_route_section_predefined_comment)"
+    local array_index=0
+    for array_index in "${!use_g16_route_section_predefined[@]}" ; do
+      (( array_index > 0 )) && printf '\n'
+      printf '%3d       : ' "$array_index" 
+      local printvar printline=0
+      while read -r printvar || [[ -n "$printvar" ]] ; do
+        if (( printline == 0 )) ; then
+          printf '%-80s\n' "$printvar"
+        else
+          printf '            %-80s\n' "$printvar"
+        fi
+        (( printline++ ))
+      done <<< "$( fold -w80 -s <<< "${use_g16_route_section_predefined[$array_index]}" )"
+      unset printvar 
+      while read -r printvar || [[ -n "$printvar" ]] ; do
+        [[ -z "$printvar" ]] && printvar="no comment"
+        printf '%3d(cmt.) : %-80s\n' "$array_index" "$printvar"
+      done <<< "$( fold -w80 -s <<< "${use_g16_route_section_predefined_comment[$array_index]}" )"
     done
     ask "Which entry would you like to set as the default?"
+    local user_input
     user_input=$(read_integer)
-    while (( user_input >= ${#tmp_g16_route_section_predefined[@]} )) ; do
+    while (( user_input >= ${#use_g16_route_section_predefined[@]} )) ; do
       user_input=$(read_integer)
     done
-    use_g16_route_section_default="${tmp_g16_route_section_predefined[$user_input]}"
-  else
-    ask "What should be the default route section?"
-    use_g16_route_section_default=$(read_human_input)
+    use_g16_route_section_default="${use_g16_route_section_predefined[$user_input]}"
+    
+    return 0
   fi
+  
+  debug "Calling old function."
+  warning "You shouldn't actually see the following part of the script. Installation might have gone wrong."
+  #  Unsetting related variables
+  unset use_g16_route_section_default
   unset use_g16_route_section_predefined
-  use_g16_route_section_predefined=( "${tmp_g16_route_section_predefined[@]}" )
-  unset tmp_g16_route_section_predefined
+  unset use_g16_route_section_predefined_comment
 }
 
 ask_stay_quiet ()
@@ -778,8 +782,10 @@ get_configuration_interactive ()
   use_g16_route_section_default="$g16_route_section_default"
   debug "g16_route_section_default=$use_g16_route_section_default"
   use_g16_route_section_predefined=( "${g16_route_section_predefined[@]}" )
+  use_g16_route_section_predefined_comment=( "${g16_route_section_predefined_comment[@]}" )
   debug "$(declare -p use_g16_route_section_predefined)"
-  if [[ ! -z $use_g16_route_section_default ]] ; then
+  debug "$(declare -p use_g16_route_section_predefined_comment)"
+  if [[ -n $use_g16_route_section_default ]] ; then
     message "Recovered setting: 'g16_route_section_default=$use_g16_route_section_default'"
   fi
   if (( ${#use_g16_route_section_predefined[@]} != 0 )) ; then
@@ -1044,11 +1050,14 @@ print_configuration ()
   fi
   if (( ${#use_g16_route_section_predefined[@]} == 0 )) ; then
     echo "# g16_route_section_predefined[0]='#P B97D3/def2-SVP'"
+    echo "# g16_route_section_predefined_comment[0]='pure DFT method, double zeta BS'"
     echo "# g16_route_section_predefined[1]='#P B97D3/def2-TZVPP OPT'"
+    echo "# g16_route_section_predefined_comment[1]='pure DFT method, triple zeta BS, optimisation'"
   else
     local array_index=0
     for array_index in "${!use_g16_route_section_predefined[@]}" ; do
       printf '  g16_route_section_predefined[%d]="%s"\n' "$array_index" "${use_g16_route_section_predefined[$array_index]}"
+      printf '  g16_route_section_predefined_comment[%d]="%s"\n' "$array_index" "${use_g16_route_section_predefined_comment[$array_index]}"
     done
   fi
   echo "#"
@@ -1249,6 +1258,7 @@ fi
 if [[ "$1" == "debug" ]] ; then
   exec 4>&1
   stay_quiet=0 
+  debugging=true
   shift 
 else
   exec 4> /dev/null
---
+++ configure/routebuilder.sh
@@ -0,0 +1,367 @@
+#!/bin/bash
+
+# This script is only meant to configure tools-for-g16.bash
+
+#
+# Generic functions to find the scripts 
+# (Copy of ./resources/locations.sh)
+#
+# Let's know where the script is and how it is actually called
+#
+
+get_absolute_location ()
+{
+    # Resolves the absolute location of parameter and returns it
+    # Taken from https://stackoverflow.com/a/246128/3180795
+    local resolve_file="$1" description="$2" 
+    local link_target directory_name filename resolve_dir_name 
+    debug "Getting directory for '$resolve_file'."
+    #  resolve $resolve_file until it is no longer a symlink
+    while [[ -h "$resolve_file" ]]; do 
+      link_target="$(readlink "$resolve_file")"
+      if [[ $link_target == /* ]]; then
+        debug "File '$resolve_file' is an absolute symlink to '$link_target'"
+        resolve_file="$link_target"
+      else
+        directory_name="$(dirname "$resolve_file")" 
+        debug "File '$resolve_file' is a relative symlink to '$link_target' (relative to '$directory_name')"
+        #  If $resolve_file was a relative symlink, we need to resolve 
+        #+ it relative to the path where the symlink file was located
+        resolve_file="$directory_name/$link_target"
+      fi
+    done
+    debug "File is '$resolve_file'" 
+    filename="$(basename "$resolve_file")"
+    debug "File name is '$filename'"
+    resolve_dir_name="$(dirname "$resolve_file")"
+    directory_name="$(cd -P "$(dirname "$resolve_file")" && pwd)"
+    if [[ "$directory_name" != "$resolve_dir_name" ]]; then
+      debug "$description '$directory_name' resolves to '$directory_name'."
+    fi
+    debug "$description is '$directory_name'"
+    if [[ -z $directory_name ]] ; then
+      directory_name="."
+    fi
+    echo "$directory_name/$filename"
+}
+
+get_absolute_filename ()
+{
+    # Returns only the filename
+    local resolve_file="$1" description="$2" return_filename
+    return_filename=$(get_absolute_location "$resolve_file" "$description")
+    return_filename=${return_filename##*/}
+    echo "$return_filename"
+}
+
+get_absolute_dirname ()
+{
+    # Returns only the directory
+    local resolve_file="$1" description="$2" return_dirname
+    return_dirname=$(get_absolute_location "$resolve_file" "$description")
+    return_dirname=${return_dirname%/*}
+    echo "$return_dirname"
+}
+
+get_scriptpath_and_source_files ()
+{
+    local error_count tmplog line 
+    tmplog=$(mktemp tmp.XXXXXXXX) 
+    # Who are we and where are we?
+    scriptname="$(get_absolute_filename "${BASH_SOURCE[0]}" "installname")"
+    debug "Script is called '$scriptname'"
+    # remove scripting ending (if present)
+    scriptbasename=${scriptname%.sh} 
+    debug "Base name of the script is '$scriptbasename'"
+    # We are in another subdirectory
+    scriptpath="$(get_absolute_dirname  "${BASH_SOURCE[0]}" "current installdirectory")"
+    # move one up
+    debug "Script is located in '$scriptpath'"
+    resourcespath="$scriptpath/../resources"
+    
+    if [[ -d "$resourcespath" ]] ; then
+      debug "Found library in '$resourcespath'."
+    else
+      (( error_count++ ))
+    fi
+    
+    # Import default variables
+    #shellcheck source=/home/te768755/devel/tools-for-g16.bash/resources/default_variables.sh
+    source "$resourcespath/default_variables.sh" &> "$tmplog" || (( error_count++ ))
+    
+    # Import other functions
+    #shellcheck source=/home/te768755/devel/tools-for-g16.bash/resources/messaging.sh
+    source "$resourcespath/messaging.sh" &> "$tmplog" || (( error_count++ ))
+    #shellcheck source=/home/te768755/devel/tools-for-g16.bash/resources/rcfiles.sh
+    source "$resourcespath/rcfiles.sh" &> "$tmplog" || (( error_count++ ))
+    #shellcheck source=/home/te768755/devel/tools-for-g16.bash/resources/test_files.sh
+    source "$resourcespath/test_files.sh" &> "$tmplog" || (( error_count++ ))
+    #shellcheck source=/home/te768755/devel/tools-for-g16.bash/resources/process_gaussian.sh
+    source "$resourcespath/process_gaussian.sh" &> "$tmplog" || (( error_count++ ))
+    #shellcheck source=/home/te768755/devel/tools-for-g16.bash/resources/validate_numbers.sh
+    source "$resourcespath/validate_numbers.sh" &> "$tmplog" || (( error_count++ ))
+
+    if (( error_count > 0 )) ; then
+      echo "ERROR: Unable to locate library functions. Check installation." >&2
+      echo "ERROR: Expect functions in '$resourcespath'."
+      debug "Errors caused by:"
+      while read -r line || [[ -n "$line" ]] ; do
+        debug "$line"
+      done < "$tmplog"
+      debug "$(rm -v -- "$tmplog")"
+      exit 1
+    else
+      debug "$(rm -v -- "$tmplog")"
+    fi
+}
+
+#
+# Read config files
+#
+
+get_configuration_from_file ()
+{
+  # Check for settings in three default locations (increasing priority):
+  #   install path of the script, user's home directory, current directory
+  g16_tools_path=$(get_absolute_dirname "$scriptpath/../g16.tools.rc")
+  local g16_tools_rc_searchlocations
+  g16_tools_rc_searchlocations=( "$g16_tools_path" "$HOME" "$HOME/.config" "$PWD" )
+  g16_tools_rc_loc="$( get_rc "${g16_tools_rc_searchlocations[@]}" )"
+  debug "g16_tools_rc_loc=$g16_tools_rc_loc"
+  
+  # Load custom settings from the rc
+  
+  if [[ -n $g16_tools_rc_loc ]] ; then
+    #shellcheck source=/home/te768755/devel/tools-for-g16.bash/g16.tools.rc 
+    . "$g16_tools_rc_loc"
+    message "Configuration file '${g16_tools_rc_loc/*$HOME/<HOME>}' applied."
+  fi
+}
+#
+# Specific functions for this script only
+#
+
+write_to_file ()
+{
+  echo "$*" >&9
+}
+
+read_index ()
+{
+  debug "Reading integer."
+  local readvar
+  until [[ $readvar =~ ^[[:space:]]*([Dd]?[[:digit:]]+[Dd]?|[Qq]|[Ss])[[:space:]]*$ ]] ; do
+    message "Which route section would you like to edit?"
+    message "To create a new route section pick an unused number."
+    message "To delete a route section suffix/append 'd' to its index number."
+    message "Enter 's' save the manipulated data to '$output_file'."
+    message "Please enter an integer value or 'q' to quit."
+    echo -n "ANSWER  : " >&3
+    read -r readvar
+  done
+  debug "Whole match is |${BASH_REMATCH[0]}|; Cleaned part is |${BASH_REMATCH[1]}|"
+  readvar="${BASH_REMATCH[1]}"
+  debug "readvar=$readvar"
+  echo "$readvar"
+}
+
+list_route_sections ()
+{
+  local array_index=0
+  for array_index in "${!g16_route_section_predefined[@]}" ; do
+    (( array_index > 0 )) && printf '\n'
+    printf '%3d       : ' "$array_index" 
+    local printvar printline=0
+    while read -r printvar || [[ -n "$printvar" ]] ; do
+      if (( printline == 0 )) ; then
+        printf '%-80s\n' "$printvar"
+      else
+        printf '            %-80s\n' "$printvar"
+      fi
+      (( printline++ ))
+    done <<< "$( fold -w80 -s <<< "${g16_route_section_predefined[$array_index]}" )"
+    unset printvar 
+    [[ -z ${g16_route_section_predefined_comment[$array_index]} ]] && g16_route_section_predefined_comment[$array_index]="(n.a.)"
+    while read -r printvar || [[ -n "$printvar" ]] ; do
+      printf '%3d(cmt.) : %-80s\n' "$array_index" "${printvar:-no comment available}"
+    done <<< "$( fold -w80 -s <<< "${g16_route_section_predefined_comment[$array_index]}" )"
+  done
+}
+
+edit_single_route ()
+{
+  echo "This feature is not yet fully implemented."
+#  sleep 1
+  local editor="${EDITOR:-vim}"
+  debug "Editor: $editor"
+  local read_route_content="$1"
+  debug "Processing route: $read_route_content"
+  local read_route_comment="$2"
+  debug "Processing comment: $read_route_comment"
+  local tmpfile_route printvar
+  tmpfile_route="$( mktemp --tmpdir )"
+  debug "Created temporary file: $tmpfile_route"
+  if command -v "$editor" > /dev/null ; then
+    exec 9> "$tmpfile_route"
+    write_to_file  "% This skript ($0) is used to configure the default route sections."
+    write_to_file  "% Lines starting with per-cent (%) will be ignored silently."
+    write_to_file  "% Lines starting with an exclamation mark (!) will be considered a comment."
+    write_to_file  "% Every other line will be considered part of the route section."
+    write_to_file  "% The route indicator may be omitted (and then defaults to #),"
+    write_to_file  "% or explicitly specified to #N/#P/#T at the __beginning__ of the route section."
+    write_to_file  "% Note that special characters may cause unforseen side-effects, as this is WIP."
+    write_to_file  "$( fold -w80 -s <<< "${read_route_content:-#}" )"
+    while read -r printvar || [[ -n "$printvar" ]] ; do
+      [[ -z "$printvar" ]] && printvar="no comment"
+      write_to_file "! $printvar"
+    done <<< "$( fold -w78 -s <<< "$read_route_comment" )" 
+    write_to_file  "% End of temporary file."
+    exec 9>&-
+    $editor "$tmpfile_route" >&1
+  else
+    fatal "Command not found: $editor" 
+  fi
+  local line pattern 
+  local return_route_startpattern return_route_content return_route_comment
+  while read -r line || [[ -n "$line" ]] ; do
+    debug "$line"
+    [[ "$line" =~ ^[[:space:]]*% ]] && { debug "Explanation line." ; continue ; }
+    [[ "$line" =~ ^[[:space:]]*![[:space:]]*(.*)$ ]] && { return_route_comment+="${BASH_REMATCH[1]} " ; debug "Comment line." ; continue ; }
+    if [[ -z $return_route_content ]] ; then
+      debug "No content in route yet."
+      pattern="^[[:space:]]*(#|#[nNpPtT]?)[[:space:]]*(.*)$"
+      if [[ "$line" =~ $pattern ]] ; then 
+        return_route_startpattern="${BASH_REMATCH[1]}" 
+        return_route_content="${BASH_REMATCH[2]} " 
+        debug "Found start pattern: $return_route_startpattern"
+        debug "Found route content: $return_route_content"
+        continue 
+      fi
+    fi
+    pattern="^[[:space:]]*#[^[:space]]*[[:space:]]*(.*)$"
+    [[ "$line" =~ $pattern ]] && line="${BASH_REMATCH[1]}"
+    return_route_content+="$line "
+  done < "$tmpfile_route"
+  debug "Comment: $return_route_comment"
+  debug "Start pattern: $return_route_startpattern"
+  debug "Route content: $return_route_content"
+  edited_route="${return_route_startpattern:-#} $return_route_content ! $return_route_comment"
+  debug "$( rm -v -- "$tmpfile_route" )"
+}
+
+### #TMP stuff for testing
+### 
+###   g16_route_section_predefined[0]="# PM6"
+###   g16_route_section_predefined_comment[0]="semi-empirical method (default route)"
+### #
+###   g16_route_section_predefined[1]="#P B97D3/def2SVP/W06                            DenFit"
+###   g16_route_section_predefined_comment[1]="pure DFT method with density-fitting, double zeta BS (default route)"
+### #
+###   g16_route_section_predefined[2]="#P B97D3/def2TZVPP/W06                          DenFit"
+###   g16_route_section_predefined_comment[2]="pure DFT method with density-fitting, triple zeta BS (default route)"
+
+#
+# MAIN SCRIPT
+#
+
+# If this script is sourced, return before executing anything
+if ( return 0 2>/dev/null ) ; then
+  # [How to detect if a script is being sourced](https://stackoverflow.com/a/28776166/3180795)
+  debug "Script is sourced. Return now."
+  return 0
+fi
+
+# Save how script was called
+printf -v script_invocation_spell "'%s' " "${0/#$HOME/<HOME>}" "$@"
+
+# Sent logging information to stdout
+exec 3>&1
+
+# Need to define debug function if unknown (It should be unknown.)
+if ! command -v debug > /dev/null ; then
+  debug () {
+    echo "DEBUG  : " "$*" >&4
+  }
+fi
+
+# Secret debugging switch
+if [[ "$1" == "debug" ]] ; then
+  exec 4>&1
+  stay_quiet=0 
+  shift 
+else
+  exec 4> /dev/null
+fi
+
+get_scriptpath_and_source_files || exit 1
+
+# Get options
+# Initialise options
+OPTIND="1"
+
+while getopts :hr:o: options ; do
+  #hlp   Usage: $scriptname [options]
+  #hlp
+  #hlp   Options:
+  #hlp
+  case $options in
+    #hlp     -h        Prints this help text
+    #hlp
+    h) echo "helpme" ;; 
+
+    #hlp     -r <ARG>  Raw mode. Do not load configuration files, use <ARG> as input. 
+    #hlp
+    r) raw_mode="true" ; raw_routes="$OPTARG" ;;
+
+    #hlp     -o <ARG>  Specify output file for created route sections
+    #hlp
+    o) output_file="$OPTARG" 
+       ;;
+
+    #hlp     --       Close reading options.
+    # This is the standard closing argument for getopts, it needs no implemenation.
+
+   \?) fatal "Invalid option: -$OPTARG." ;;
+
+    :) fatal "Option -$OPTARG requires an argument." ;;
+
+  esac
+done
+
+if [[ $raw_mode == "true" ]] ; then
+  unset g16_route_section_predefined g16_route_section_predefined_comment
+  . "$raw_routes"
+else
+  get_configuration_from_file 
+fi
+output_file="${output_file:-$(mktemp)}"
+
+list_route_sections 
+
+while index=$( read_index ) ; do
+  debug "Index chosen: $index"
+  [[ "$index" == "q" || "$index" == "Q" ]] && exit 0
+  if [[ "$index" == "s" || "$index" == "S" ]] ; then
+    {
+      echo "# Following route sections created:"
+      declare -p g16_route_section_predefined
+      declare -p g16_route_section_predefined_comment
+      echo "# done"
+    } > "$output_file"
+    exit 0
+  fi
+  if [[ "$index" =~ [Dd]+ ]] ; then
+    index="${index//[^[:digit:]]/}"
+    unset "g16_route_section_predefined[$index]"
+    unset "g16_route_section_predefined_comment[$index]"
+  else
+    edit_single_route "${g16_route_section_predefined[$index]}" "${g16_route_section_predefined_comment[$index]}" 
+    g16_route_section_predefined[$index]=${edited_route%%!*}
+    g16_route_section_predefined_comment[$index]=${edited_route#*!}
+  fi
+  list_route_sections
+done
+
+message "$scriptname is part of $softwarename $version ($versiondate)"
+debug "$script_invocation_spell"
+
